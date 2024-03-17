ae264814ecbb207e17e478ca0aebab86cf028c3a
SungJaeGwon <gwonsj94@naver.com>
2024-03-17 22:33:01 +0900

    최종사이트배포

--- src/wrap/HeaderComponent.jsx
+++ src/wrap/HeaderComponent.jsx
@@ -1,51 +1,63 @@
 import React from 'react';
 import './css/header.scss';
+import { Outlet } from 'react-router-dom';
 
-export default function HeaderComponent () {
+export default function HeaderComponent() {
+  const headerFix = React.useRef();
+  const [state, setState] = React.useState({
+    isFixed: false,
+  });
 
-    const headerFix = React.useRef();
-    const [state, setState] = React.useState({
-        isFixed : false
+  React.useEffect(() => {
+    let headerHeight = headerFix.current.offsetTop + 79;
+    let isFixed = false;
+    window.addEventListener('scroll', function () {
+      if (window.scrollY >= headerHeight) {
+        isFixed = true;
+      } else {
+        isFixed = false;
+      }
+      setState({
+        ...state,
+        isFixed: isFixed,
+      });
     });
+  }, []);
 
-    
-    React.useEffect(()=>{
-        let headerHeight = headerFix.current.offsetTop+79;
-        let isFixed = false;
-        window.addEventListener('scroll',function(){
-            if( window.scrollY >= headerHeight ){
-                isFixed = true
-            }
-            else {
-                isFixed = false
-            }
-            setState({
-                ...state,
-                isFixed: isFixed
-            })
-        });
-    },[]);
+  const onClickReset = () => {};
 
-    const onClickReset= ()=> {}
-    
-
-    return (
-        <header id="header" className={state.isFixed ? 'fixed' : ''} ref={headerFix}>
-            <div className="container">
-                <div className="top">
-                    <div className="myName">
-                        <h1 onClick={onClickReset}>SungJae Gwon</h1>
-                    </div>
-                    <nav className="nav">
-                        <ul>
-                            <li><a href="#introduce">Introduce</a></li>
-                            <li><a href="#aboutme">About Me</a></li>
-                            <li><a href="#skills">Skills</a></li>
-                            <li><a href="#portfolio">Portfolio</a></li>
-                        </ul>
-                    </nav>
-                </div>
+  return (
+    <>
+      <header
+        id="header"
+        className={state.isFixed ? 'fixed' : ''}
+        ref={headerFix}
+      >
+        <div className="container">
+          <div className="top">
+            <div className="myName">
+              <h1 onClick={onClickReset}>SungJae Gwon</h1>
             </div>
-        </header>
-    );
-};
+            <nav className="nav">
+              <ul>
+                <li>
+                  <a href="#introduce">Introduce</a>
+                </li>
+                <li>
+                  <a href="#aboutme">About Me</a>
+                </li>
+                <li>
+                  <a href="#skills">Skills</a>
+                </li>
+                <li>
+                  <a href="#portfolio">Portfolio</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <Outlet />
+    </>
+  );
+}
